527ba01f8be09d8fa079628e4b7984384c27d7db
angelcode119 <mpouanfar@gmail.com>
2025-12-13 01:34:51 +0330

    Add debugPrint import for error handling

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show debugPrint;
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
